1c9c82376483c356fe9ace5cf45d98583935e8af
i-bilobran <ihor.bilobran@sombrainc.com>
2019-12-07 21:12:12 +0200

    cv card component

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -8,6 +8,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
 	declarations: [],
@@ -19,7 +20,8 @@ import { MatInputModule } from '@angular/material/input';
 		MatToolbarModule,
 		MatIconModule,
 		MatFormFieldModule,
-		MatInputModule
+		MatInputModule,
+		MatCardModule
 	],
 	exports: [
 		MatMenuModule,
@@ -28,7 +30,8 @@ import { MatInputModule } from '@angular/material/input';
 		MatToolbarModule,
 		MatIconModule,
 		MatFormFieldModule,
-		MatInputModule
+		MatInputModule,
+		MatCardModule
 	]
 })
 export class MaterialModule { }
