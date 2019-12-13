171b63c3de94660e8241c9bef97d14f5e35e17fb
i-bilobran <ihor.bilobran@sombrainc.com>
2019-12-13 18:31:24 +0200

    new resume redirect

--- src/app/components/cv-new-card/cv-new-card.component.ts
+++ src/app/components/cv-new-card/cv-new-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-cv-new-card',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CvNewCardComponent implements OnInit {
 
-	constructor() { }
+	constructor(private router: Router) { }
 
 	ngOnInit() {
 	}
 
+	public createNew(): void {
+		this.router.navigate(['/new-resume']);
+	}
 }
