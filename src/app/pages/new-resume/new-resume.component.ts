d7d52b43b9cfac7f4107e3cc7708baf3eb11c41e
IhorBilobran <ihor.bilobran@gmail.com>
2019-12-25 22:45:59 +0200

    new resume aside layout

--- src/app/pages/new-resume/new-resume.component.ts
+++ src/app/pages/new-resume/new-resume.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewResumeComponent implements OnInit {
   public contactEdit: boolean;
+  public locationEdit: boolean;
 
   constructor() { }
 
