2adfa59e2e8c741bee100fa9bc34195d1d1bbca1
IhorBilobran <ihor.bilobran@gmail.com>
2019-12-25 21:21:15 +0200

    new resume layout and contact edit

--- src/app/pages/new-resume/new-resume.component.ts
+++ src/app/pages/new-resume/new-resume.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-resume.component.scss']
 })
 export class NewResumeComponent implements OnInit {
+  public contactEdit: boolean;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleContactEdit(): void {
+    this.contactEdit = !this.contactEdit;
   }
 
 }
