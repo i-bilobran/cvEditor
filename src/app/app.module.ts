89b12ff26566a9d3be3106960fad8356124f2fa7
i-bilobran <ihor.bilobran@sombrainc.com>
2020-01-24 20:08:12 +0200

    angularx-social-login install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './modules/shared/shared.module';
@@ -8,6 +10,17 @@ import { AuthModule } from './modules/auth/auth.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 import { AppComponent } from './app.component';
+
+const config = new AuthServiceConfig([
+	{
+		id: GoogleLoginProvider.PROVIDER_ID,
+		provider: new GoogleLoginProvider('365081571287-eoq9v835kse4lja6qvpnbhsutkhlqqv5.apps.googleusercontent.com')
+	}
+]);
+
+export function provideConfig() {
+	return config;
+}
 
 @NgModule({
 	declarations: [
@@ -17,11 +30,17 @@ import { AppComponent } from './app.component';
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
+		SocialLoginModule,
 		SharedModule,
 		AuthModule,
 		DashboardModule
 	],
-	providers: [],
+	providers: [
+		{
+			provide: AuthServiceConfig,
+			useFactory: provideConfig
+		}
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { AuthService, GoogleLoginProvider, LoginOpt } from 'angularx-social-login';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	constructor(private authService: AuthService) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		// this.authService.signOut();
+		// this.signInWithGoogle();
+		// this.signInWithGoogle()
+
+		// this.authService.authState.subscribe((user) => {
+		// 	console.log(user)
+		// });
+	}
+
+	signInWithGoogle(): void {
+		this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+
+	}
 
 }
