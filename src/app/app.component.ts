37bdae5c0babebf0c30e79c8dd317ec0709019fd
i-bilobran <ihor.bilobran@sombrainc.com>
2020-01-24 19:51:09 +0200

    removing gapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { AuthService } from './modules/auth/auth.service';
 
 @Component({
 	selector: 'app-root',
 	template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
-	constructor(private auth: AuthService) {
-		this.auth.signIn();
-
+	constructor() {
 	}
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,45 +1,19 @@
-import { Injectable, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthService {
-	private auth2: any;
-
-	constructor(private zone: NgZone) { }
+	constructor() { }
 
 	public signIn(): void {
-		gapi.load('auth2', () => {
-			gapi.auth2.init({
-				client_id: '365081571287-eoq9v835kse4lja6qvpnbhsutkhlqqv5.apps.googleusercontent.com',
-				fetch_basic_profile: true
-			}).then((auth) => {
-				this.zone.run(() => {
-					const GoogleAuth = gapi.auth2.getAuthInstance();
-					this.auth2 = gapi.auth2;
 
-					// console.log(this.auth2.currentUser.get())
-
-					// access_denied ?
-					// The user denied the permission to the scopes required.
-
-					// this.auth2.signgIn().then(user => {
-					// 	console.log(user, user.getBasicProfile())
-					// })
-					// this.auth2.signgIn().then(user => {
-					// 	console.log(user)
-					// })
-				});
-			},
-			);
-		});
 	}
 
 	public isSignedIn(): boolean {
-		return this.auth2.isSignedIn.get();
+		return true;
 	}
 
 	public signOut(): void {
-		this.auth2.signOut();
 	}
 }
