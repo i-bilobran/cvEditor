07869d6e4f0205b84a5bce80832efd7abe0df9d7
i-bilobran <ihor.bilobran@sombrainc.com>
2020-01-25 18:08:40 +0200

    auth service, gapi authentication functionality

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,19 +1,58 @@
 import { Injectable } from '@angular/core';
+import { SocialUser, AuthService, GoogleLoginProvider } from 'angularx-social-login';
+import { from, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { UserService } from '../../services/user.service';
 
 @Injectable({
 	providedIn: 'root'
 })
-export class AuthService {
-	constructor() { }
-
-	public signIn(): void {
-
+export class AuthenticationService {
+	constructor(
+		private authService: AuthService,
+		private userService: UserService) {
 	}
 
-	public isSignedIn(): boolean {
-		return true;
+	public authStatus(): Observable<boolean> {
+		return this.authService.authState
+			.pipe(
+				map<SocialUser, boolean>((response: SocialUser) => {
+					if (response) {
+						this.userService.setUserData(response);
+					} else {
+						this.userService.clearUserData();
+					}
+
+					return !!response;
+				})
+			)
+	}
+
+	public signIn(): void {
+		from(this.signInWithGoogle())
+			.pipe(
+				map((d) => {
+					console.log(d);
+				}),
+				catchError((error) => {
+					console.log(error);
+					return error;
+				})
+			)
+			.subscribe((response: SocialUser) => {
+				this.userService.setUserData(response);
+			});
 	}
 
 	public signOut(): void {
+		this.authService.signOut();
+		this.userService.clearUserData();
+	}
+
+	private async signInWithGoogle(): Promise<SocialUser> {
+		const user = await this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+
+		return user;
 	}
 }
