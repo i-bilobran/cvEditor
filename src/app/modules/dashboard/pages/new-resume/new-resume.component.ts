e7c2bdf03da241c086999d750579e805c5c04fa8
IhorBilobran <ihor.bilobran@gmail.com>
2020-03-14 00:46:44 +0200

    file drop functionality

--- src/app/modules/dashboard/pages/new-resume/new-resume.component.ts
+++ src/app/modules/dashboard/pages/new-resume/new-resume.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
+import { UploadEvent, FileSystemFileEntry } from 'ngx-file-drop';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-new-resume',
@@ -9,12 +11,42 @@ export class NewResumeComponent implements OnInit {
   public contactEdit: boolean;
   public locationEdit: boolean;
 
-  constructor() { }
+  public imgUrl: SafeUrl;
+
+  constructor(
+    private chDetectorRef: ChangeDetectorRef,
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
   }
 
   public toggleAdititionalEdit(key: string): void {
     this[`${key}Edit`] = !this[`${key}Edit`];
+  }
+
+  public dropped(event: UploadEvent) {
+
+    for (const droppedFile of event.files) {
+
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+
+        fileEntry.file((file: File) => {
+          const reader = new FileReader();
+
+          reader.readAsDataURL(file);
+
+          reader.onload = () => {
+            this.sanitizer.sanitize(2, `url(${reader.result})`);
+            this.imgUrl = reader.result;
+            this.chDetectorRef.markForCheck();
+          };
+
+        })
+      }
+    }
+
   }
 }
