c12deb89b9f430f08b45c56e52deceeed6783119
i-bilobran <ihor.bilobran@sombrainc.com>
2020-03-21 12:14:11 +0200

    rseume formly-form init

--- src/app/modules/dashboard/components/resume-form/resume-form.component.ts
+++ src/app/modules/dashboard/components/resume-form/resume-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
 	selector: 'app-resume-form',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./resume-form.component.scss']
 })
 export class ResumeFormComponent implements OnInit {
+	form = new FormGroup({});
+	model = { email: 'email@gmail.com' };
+	fields: FormlyFieldConfig[] = [
+		{
+			key: 'email',
+			type: 'input',
+			templateOptions: {
+				label: 'Email address',
+				placeholder: 'Enter email',
+				required: true,
+			}
+		}
+	];
 
 	constructor() { }
 
 	ngOnInit() {
 	}
 
+
+
+	onSubmit() {
+		console.log(this.model);
+	}
 }
