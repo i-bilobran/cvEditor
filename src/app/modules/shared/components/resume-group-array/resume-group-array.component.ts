2fb528bde8804c3a3af79f22e5d063012586d974
i-bilobran <ihor.bilobran@sombrainc.com>
2020-03-21 18:01:49 +0200

    formly repeat group component

---
+++ src/app/modules/shared/components/resume-group-array/resume-group-array.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { FieldArrayType } from '@ngx-formly/core';
+
+@Component({
+	selector: 'app-resume-group-array',
+	templateUrl: './resume-group-array.component.html',
+	styleUrls: ['./resume-group-array.component.scss']
+})
+export class ResumeGroupArrayComponent extends FieldArrayType { }
