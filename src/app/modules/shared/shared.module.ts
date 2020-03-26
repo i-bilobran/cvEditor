08f63ffc17768726b8deb5480120f0b8758703f3
i-bilobran <ihor.bilobran@sombrainc.com>
2020-03-26 17:33:03 +0200

    material module location

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -8,7 +8,7 @@ import { FormlyMaterialModule } from '@ngx-formly/material';
 import { FileDropModule } from 'ngx-file-drop';
 
 import { DefaultLayoutComponent } from './components/default-layout/default-layout.component';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from '../material/material.module';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { CvCardComponent } from './components/cv-card/cv-card.component';
 import { CvNewCardComponent } from './components/cv-new-card/cv-new-card.component';
