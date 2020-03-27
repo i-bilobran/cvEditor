797701611dff1334c8246447e727550b3e8b2d88
i-bilobran <ihor.bilobran@sombrainc.com>
2020-03-27 22:48:39 +0200

    store service, firestore CRUD API

---
+++ src/app/services/store.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class StoreService {
+	private collection = 'resumes';
+
+	constructor(
+		private db: AngularFirestore
+	) { }
+
+	public getResumeCards(archived: boolean): Observable<any[]> {
+		// add query function
+		return this.getResumes()
+			.pipe(
+				map((response: any[]) => response.filter((resume: any) => resume.archived === archived))
+			);
+	}
+
+	public getResume(id: string): Observable<any> {
+		// actual data = response[i].payload.doc.data()
+		// id = response[i].payload.doc.id
+
+		return this.db
+			.collection(this.collection)
+			.doc(id).get();
+
+	}
+
+	public getResumes(): Observable<any> {
+		// actual data = response[i].payload.doc.data()
+		// id = response[i].payload.doc.id
+
+		return this.db.collection(this.collection)
+			.snapshotChanges()
+			.pipe(map((e) => {
+				return e;
+			}));
+	}
+
+	public createResume(resume: any): Observable<any> {
+		return from(
+			this.db
+				.collection(this.collection)
+				.add(resume)
+		);
+	}
+
+	public updateResume(id: string, body: any): Observable<void> {
+		return from(
+			this.db
+				.collection(this.collection)
+				.doc(id)
+				.set(body, { merge: true })
+		);
+	}
+
+	public deleteResume(id: string): Observable<void> {
+		return from(
+			this.db
+				.collection(this.collection)
+				.doc(id)
+				.delete()
+		);
+	}
+
+	public archiveResume(id: string): Observable<void> {
+		return from(
+			this.db
+				.collection(this.collection)
+				.doc(id)
+				.set({ archived: true }, { merge: true })
+		);
+	}
+
+	public downloadResume(): void {
+
+	}
+
+	private fromFirebase(payload: any): any {
+		return payload;
+	}
+}
