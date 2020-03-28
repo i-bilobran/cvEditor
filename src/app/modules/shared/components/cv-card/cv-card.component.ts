71ecc0cfb613d79b19628d7f767f9d3e362201ae
i-bilobran <ihor.bilobran@sombrainc.com>
2020-03-28 20:39:52 +0200

    resume card actions

--- src/app/modules/shared/components/cv-card/cv-card.component.ts
+++ src/app/modules/shared/components/cv-card/cv-card.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ResumeCard } from '@models/resume.models';
 
 @Component({
-  selector: 'app-cv-card',
-  templateUrl: './cv-card.component.html',
-  styleUrls: ['./cv-card.component.scss']
+	selector: 'app-cv-card',
+	templateUrl: './cv-card.component.html',
+	styleUrls: ['./cv-card.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CvCardComponent implements OnInit {
+export class CvCardComponent {
+	@Input() card: ResumeCard;
+	@Output() delete: EventEmitter<string> = new EventEmitter();
+	@Output() archive: EventEmitter<string> = new EventEmitter();
+	@Output() download: EventEmitter<string> = new EventEmitter();
 
-  constructor() { }
+	constructor(
+		private router: Router
+	) { }
 
-  ngOnInit() {
-  }
+	public onEdit(): void {
+		this.router.navigate([`/dashboard/resume/${this.card.id}`]);
+	}
+
+	public onDelete(): void {
+		this.delete.emit(this.card.id);
+	}
+
+	public onArchive(): void {
+		this.archive.emit(this.card.id);
+	}
+
+	public onDownload(): void {
+		this.download.emit(this.card.id);
+	}
 
 }
