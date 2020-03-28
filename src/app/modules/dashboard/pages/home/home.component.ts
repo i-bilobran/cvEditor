7ebdf83f5097e48b723c5586bb1f0fee048c0361
i-bilobran <ihor.bilobran@sombrainc.com>
2020-03-28 20:39:57 +0200

    home page functionality

--- src/app/modules/dashboard/pages/home/home.component.ts
+++ src/app/modules/dashboard/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+
 import { StoreService } from '@services/store.service';
 import { ResumeCard } from '@models/resume.models';
 
@@ -16,14 +17,32 @@ export class HomeComponent implements OnInit {
 	) { }
 
 	ngOnInit() {
-		this.initResumeCards();
+		this.getResumeCards();
 	}
 
-	private initResumeCards(): void {
+	public deleteResume(id: string): void {
+		this.store.deleteResume(id)
+			.subscribe(() => {
+				console.log('Success');
+			});
+	}
+
+	public archiveResume(id: string): void {
+		this.store.archiveResume(id)
+			.subscribe(() => {
+				console.log('Success');
+			});
+	}
+
+	public downloadResume(id: string): void {
+
+	}
+
+	private getResumeCards(): void {
 		this.store.getResumeCards(false)
 			.subscribe((response: ResumeCard[]) => {
 				this.resumeCards = response;
 				this.changeDetectorRef.markForCheck();
-			})
+			});
 	}
 }
