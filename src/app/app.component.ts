378b1a33f081e25390b0f31608a226ef61898147
i-bilobran <ihor.bilobran@sombrainc.com>
2020-03-29 18:59:58 +0300

    add connectionService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { ConnectionService } from 'ng-connection-service';
 
 @Component({
 	selector: 'app-root',
 	template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
-	constructor() { }
+	public isConnected = true;
+
+	constructor(private connectionService: ConnectionService) {
+		this.connectionService.monitor().subscribe(isConnected => {
+			console.log('connection: ', isConnected);
+			this.isConnected = isConnected;
+		});
+	}
 }
