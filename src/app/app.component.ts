462984aadcd5343ccb7c04c50182f97bff68f59e
i-bilobran <ihor.bilobran@sombrainc.com>
2020-03-29 19:51:38 +0300

    static resources configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ConnectionService } from 'ng-connection-service';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
 	selector: 'app-root',
@@ -8,7 +9,15 @@ import { ConnectionService } from 'ng-connection-service';
 export class AppComponent {
 	public isConnected = true;
 
-	constructor(private connectionService: ConnectionService) {
+	constructor(
+		private connectionService: ConnectionService,
+		private updates: SwUpdate
+	) {
+		updates.available.subscribe(() => updates.activateUpdate().then(() => {
+			alert('sw, update')
+			document.location.reload();
+		}));
+
 		this.connectionService.monitor().subscribe(isConnected => {
 			console.log('connection: ', isConnected);
 			this.isConnected = isConnected;
