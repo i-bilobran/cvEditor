367e930ef516d8671d745ee8ddf997e7fa1786d2
i-bilobran <ihor.bilobran@sombrainc.com>
2020-06-02 21:28:16 +0300

    base Page Overload handling

--- src/app/modules/shared/components/resume-preview/resume-preview.component.ts
+++ src/app/modules/shared/components/resume-preview/resume-preview.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef, SimpleChanges } from '@angular/core';
 
-import { Resume } from '@models/resume.models';
+import { Resume, ResumeForm, About } from '@models/resume.models';
+import { clone, cloneDeep } from 'lodash';
 
 @Component({
 	selector: 'app-resume-preview',
@@ -8,11 +9,17 @@ import { Resume } from '@models/resume.models';
 	styleUrls: ['./resume-preview.component.scss']
 })
 export class ResumePreviewComponent implements OnInit, AfterViewInit {
-	@Input() pages: Resume[];
-	@ViewChild('wrapper', { static: true }) element: ElementRef<any>;
+	@Input() base: Resume[];
 
-	constructor() { }
+	public pages: Resume[];
 
+	constructor(private cd: ChangeDetectorRef) { }
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (!this.pages && changes.base.currentValue) {
+			this.pages = cloneDeep(changes.base.currentValue);
+		}
+	}
 	// Array.from(temp1.childNodes).filter(el => el.hasChildNodes())
 
 	// recursion
@@ -27,10 +34,68 @@ export class ResumePreviewComponent implements OnInit, AfterViewInit {
 	// else hidden => false
 
 	ngOnInit() {
-		console.log(this.pages[0])
+		console.log(this.base[0])
 	}
 
 	ngAfterViewInit() {
-		console.log(document.getElementsByClassName('pdf-page'))
+		const pages = Array.from(document.getElementsByClassName('pdf-page'));
+		const lastPage = pages[pages.length - 1];
+		const items = Array.from(lastPage.getElementsByClassName('main')[0].childNodes).filter(el => el.hasChildNodes());
+
+		let currentHeight = 0;
+
+		items.forEach((item: HTMLElement, index: number) => {
+			currentHeight += item.offsetHeight;
+			const model = item.getAttribute('data-model');
+
+			if (currentHeight <= 1133) {
+				this.handlePageOverload(model, false);
+			} else {
+				this.handlePageOverload(model, true);
+			}
+		});
+
+		this.cd.detectChanges();
+	}
+
+	private handlePageOverload(model: string, removal: boolean): void {
+		const basePage = this.pages[0];
+		let newPage = this.pages[0 + 1];
+
+		if (model !== 'fullInfo' && model !== 'education') {
+			if (removal) {
+				const newField = {} as ResumeForm;
+
+				newField[model] = clone(basePage.resume[model]);
+
+				if (!newPage) {
+					newPage = clone(basePage);
+					newPage.resume = newField;
+				} else {
+					newPage.resume[model] = basePage.resume[model];
+				}
+
+				basePage.resume[model] = null;
+			} else {
+
+			}
+		} else {
+			if (removal) {
+				const newField = basePage.resume.about[model];
+
+				if (!newPage) {
+					newPage = clone(basePage);
+					newPage.resume.about[model] = newField;
+				} else if (newPage && !newPage.resume.about) {
+					newPage.resume.about = {} as About;
+					newPage.resume.about[model] = newField;
+				} else {
+					newPage.resume.about[model] = newField;
+				}
+
+				basePage.resume.about[model] = null;
+			} else {
+			}
+		}
 	}
 }
